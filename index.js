848ed5c4f090a1f965f7d37edba6d92ff46e85ae
suneo <esuesu28903@gmail.com>
2024-12-24 00:44:51 +0900

    Database Tutorial

--- index.js
+++ index.js
@@ -1,59 +1,9 @@
-/*const http = require("node:http");
-
-const hostname = "127.0.0.1";
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello, World!\n");
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});*/
-
-/*const http = require("node:http");
-let fs = require("fs");
-const mimeTypes = {
-  ".html": "text/html",
-  ".css": "text/css",
-  ".js": "text/javascript",
-  ".png": "image/png",
-  ".jpg": "image/jpeg",
-  ".gif": "image/gif",
-  ".svg": "image/svg+xml",
-  ".ico": "image/x-icon",
-};
-
-const hostname = "127.0.0.1";
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    fs.readFile("./index.html", "UTF-8", (error, data) => {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      res.end();
-    });
-  } else if (req.url === "/test") {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end("Hello, World! root/test\n");
-  } else {
-    res.statusCode = 400;
-    res.setHeader("Content-Type", "text/plain");
-    res.end("404 Not Found\n");
-  }
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});*/
-
-const http = require("node:http");
+const http = require("http");
 const fs = require("fs");
 const path = require("path");
+const url = require("url");
+const querystring = require("querystring");
+const sqlite3 = require("sqlite3").verbose();
 
 const mimeTypes = {
   ".html": "text/html",
@@ -68,6 +18,26 @@ const mimeTypes = {
 
 const hostname = "127.0.0.1";
 const port = 3000;
+
+function serveStaticFile(filePath, res) {
+  const extname = String(path.extname(filePath)).toLowerCase();
+  const contentType = mimeTypes[extname] || "application/octet-stream";
+
+  fs.readFile(filePath, (error, content) => {
+    if (error) {
+      if (error.code == "ENOENT") {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("404 Not Fount", "utf-8");
+      } else {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("500 Internal Server Error", "utf-8");
+      }
+    } else {
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content, "utf-8");
+    }
+  });
+}
 
 const server = http.createServer((req, res) => {
   let filePath = "." + req.url; // リクエストされたURLを基にファイルパスを作成
@@ -78,6 +48,8 @@ const server = http.createServer((req, res) => {
   } else if (filePath === "./user_") {
     filePath = "./user_index/user_index.html";
     console.log(filePath);
+  } else if (req.url === "/add-user" && req.method === "POST") {
+    let body = "";
   }
 
   // ファイルの拡張子を取得
